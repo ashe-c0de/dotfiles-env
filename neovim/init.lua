d818f602346ebd65c311069e55fbadb3fa12fd83
Ashe <ashet@qq.com>
2026-03-24 23:59:39 +0800

    Update init.lua

--- neovim/init.lua
+++ neovim/init.lua
@@ -190,13 +190,21 @@ require("lazy").setup({
     "nvim-neo-tree/neo-tree.nvim",
     branch = "v3.x",
     dependencies = {
-      "nvim-lua/plenary.nvim",
-      "nvim-tree/nvim-web-devicons",
-      "MunifTanjim/nui.nvim"
+    "nvim-lua/plenary.nvim",
+    "nvim-tree/nvim-web-devicons",
+    "MunifTanjim/nui.nvim"
     },
-    config = function()
+    config = function ()
       require("neo-tree").setup({
         close_if_last_window = true,
+          filesystem = {
+            filtered_items = {
+            visible = true,
+            hide_dotfiles = false, 
+            hide_gitignored = false,
+            never_show = {},
+          },
+        },
         window = { mappings = { ["<space>"] = "none" } }
       })
       vim.keymap.set("n", "<leader>e", "<cmd>Neotree toggle<CR>")
