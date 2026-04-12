57727bba8f923831efb46228172d48e6e515faf8
Ashe <ashet@qq.com>
2026-04-12 14:49:46 +0800

    Update and rename .wezterm.lua to for-windows-.wezterm.lua

--- wezterm/for-windows-.wezterm.lua
+++ wezterm/for-windows-.wezterm.lua
@@ -1,8 +1,11 @@
 local wezterm = require 'wezterm'
 local config = wezterm.config_builder()
 
+-- scoop install pwsh
+config.default_prog = { "pwsh" }
+
 config.font = wezterm.font("JetBrains Mono")
-config.font_size = 13.0
+config.font_size = 15.0
 config.window_background_opacity = 1.0
 
 -- tab
